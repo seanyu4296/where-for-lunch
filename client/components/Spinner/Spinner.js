10acbbcb9fb236db7db25b527bb126062051cd7f
seanyu4296 <seanyu4296@gmail.com>
2018-07-12 00:55:34 +0800

    Fix lint errors

--- client/components/Spinner/Spinner.js
+++ client/components/Spinner/Spinner.js
@@ -3,11 +3,11 @@ import styles from './Spinner.css';
 
 export default () => (
   <div className={styles.root}>
-  <div className={styles['lds-ring']}>
-    <div />
-    <div />
-    <div />
-    <div />
-  </div>
+    <div className={styles['lds-ring']}>
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
   </div>
 );
